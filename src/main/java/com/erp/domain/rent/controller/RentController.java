5342d1b83d212cc404a8f41e67fbacd16eaeb8db
hamin <kyden5386@gmail.com>
2026-01-12 16:13:11 +0900

    Chore: remove unused import

--- src/main/java/com/erp/domain/rent/controller/RentController.java
+++ src/main/java/com/erp/domain/rent/controller/RentController.java
@@ -2,7 +2,6 @@ package com.erp.domain.rent.controller;
 
 import com.erp.domain.branch.dto.request.BranchSearchRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
