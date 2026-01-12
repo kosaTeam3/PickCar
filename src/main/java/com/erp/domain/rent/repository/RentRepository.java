53b6336e4a56a15e3aab14d9682d98dd0bd680e1
DESKTOP-21MO7C7\Admin <bigwin0207@naver.com>
2026-01-12 15:07:18 +0900

    feat: 회원 목록, 상세 정보, 렌트, 사고, 블랙리스트 이력 조회

--- src/main/java/com/erp/domain/rent/repository/RentRepository.java
+++ src/main/java/com/erp/domain/rent/repository/RentRepository.java
@@ -3,7 +3,6 @@ package com.erp.domain.rent.repository;
 import com.erp.domain.rent.entity.Rent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface RentRepository extends JpaRepository<Rent, Long> {
