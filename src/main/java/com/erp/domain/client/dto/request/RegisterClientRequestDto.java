d12c72a2a757f8499b47257dc7b46b6c924a2b98
coder_zero <berichmore@naver.com>
2026-01-13 18:21:19 +0900

    Docs : 필요없는 import 제거

--- src/main/java/com/erp/domain/client/dto/request/RegisterClientRequestDto.java
+++ src/main/java/com/erp/domain/client/dto/request/RegisterClientRequestDto.java
@@ -1,6 +1,5 @@
 package com.erp.domain.client.dto.request;
 
-import com.erp.domain.client.entity.Gender;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
