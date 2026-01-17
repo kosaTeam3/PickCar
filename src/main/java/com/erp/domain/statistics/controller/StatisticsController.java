07abdd19b831fa4b7e2816bc76f192c749993bc1
KIMHYEJIN <k.hyejin1030@gmail.com>
2026-01-17 14:44:44 +0900

    Refactor: 통계 API 요청 파라미터 타입 변경

--- src/main/java/com/erp/domain/statistics/controller/StatisticsController.java
+++ src/main/java/com/erp/domain/statistics/controller/StatisticsController.java
@@ -3,11 +3,14 @@ package com.erp.domain.statistics.controller;
 import com.erp.domain.statistics.dto.StatisticsResponse;
 import com.erp.domain.statistics.service.StatisticsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.List;
 
 @RestController
@@ -20,8 +23,8 @@ public class StatisticsController {
     /* 월간 대여 건수 조회 */
     @GetMapping("/case/month")
     public List<StatisticsResponse> getMonthlyCaseStats(
-            @RequestParam String startMonth,
-            @RequestParam String endMonth
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM") YearMonth startMonth,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM") YearMonth endMonth
     ) {
         return statisticsService.getMonthlyRentStats(startMonth, endMonth);
     }
@@ -29,8 +32,8 @@ public class StatisticsController {
     /* 일간 대여 건수 조회 */
     @GetMapping("/case/day")
     public List<StatisticsResponse> getDailyCaseStats(
-            @RequestParam String startDate, // 예: 2026-01-01
-            @RequestParam String endDate    // 예: 2026-01-31
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate
     ) {
         return statisticsService.getDailyRentStats(startDate, endDate);
     }
@@ -38,8 +41,8 @@ public class StatisticsController {
     /* 주간 대여 건수 조회 */
     @GetMapping("/case/week")
     public List<StatisticsResponse> getWeeklyCaseStats(
-            @RequestParam String startDate,
-            @RequestParam String endDate
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate
     ) {
         return statisticsService.getWeeklyRentStats(startDate, endDate);
     }
