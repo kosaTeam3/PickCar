b9127ffffc8b89e6e6ed0da07c1a02b80c9123fb
hamin <kyden5386@gmail.com>
2026-01-18 21:20:51 +0900

    chore: 회원 렌트 이력 controller 주석 처리

--- src/main/java/com/erp/domain/client/controller/ClientController.java
+++ src/main/java/com/erp/domain/client/controller/ClientController.java
@@ -35,11 +35,11 @@ public class ClientController {
     public ClientDetailResponse getClient(@PathVariable Long clientId) {
         return clientService.getClientDetail(clientId);
     }
-
-    @GetMapping("/rent/{clientId}")
-    public List<ClientRentHistoryResponse> getRentHistory(@PathVariable Long clientId) {
-        return clientService.getRentHistory(clientId);
-    }
+    // todo
+//    @GetMapping("/rent/{clientId}")
+//    public List<ClientRentHistoryResponse> getRentHistory(@PathVariable Long clientId) {
+//        return clientService.getRentHistory(clientId);
+//    }
     // todo
 //    @GetMapping("/accident/{clientId}")
 //    public List<ClientAccidentHistoryResponse> getAccidentHistory(@PathVariable Long clientId) {
