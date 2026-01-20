c9a04408aea782dacdd4f3cfc59a7ecd14bb6b29
DESKTOP-21MO7C7\Admin <bigwin0207@naver.com>
2026-01-20 10:47:46 +0900

    Refactor: 블랙리스트 예외처리 제외

--- src/main/java/com/erp/domain/client/service/ManagerClientService.java
+++ src/main/java/com/erp/domain/client/service/ManagerClientService.java
@@ -142,11 +142,6 @@ public class ManagerClientService {
         Client client = clientRepository.findById(clientId)
                 .orElseThrow(() -> new CustomException(404, "해당 고객을 찾을 수 없습니다."));
 
-        boolean isBlack = client.getBlacklisted() != null && client.getBlacklisted();
-        if (!isBlack) {
-            throw new CustomException(400, "이미 블랙리스트 해제 상태인 고객입니다.");
-        }
-
         client.setBlacklisted(false);
         client.setBlacklistInfo(null);
     }
