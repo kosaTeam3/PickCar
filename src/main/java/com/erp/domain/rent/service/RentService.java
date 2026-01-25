1ba1941d0efac1ce923ec52d5a954d8caf1a84d9
hamin <kyden5386@gmail.com>
2026-01-25 23:03:44 +0900

    Refactor: 렌트중인 차량 조회에 인도 여부 추가

--- src/main/java/com/erp/domain/rent/service/RentService.java
+++ src/main/java/com/erp/domain/rent/service/RentService.java
@@ -178,6 +178,7 @@ public class RentService {
                 .endRentDateTime(rent.getEndRentDateTime())
                 .branchName(rent.getCar().getBranch().getName())
                 .branchAddress(rent.getCar().getBranch().getAddress())
+                .isPickedUp(rent.getCar().getStatus() == com.erp.domain.car.entity.CarStatus.DRIVING)
                 .build();
     }
 
